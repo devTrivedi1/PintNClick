c9eb948b158109318bc0a1fe5c7f14111879f179
Dev <d.trivedi@saedubai.com>
2022-10-30 14:44:47 +0400

    avoids objects now

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-	[SerializeField] Transform moveToTarget;
-	[SerializeField] Camera gameCamera;
-	[SerializeField] LayerMask ground;
-
+	[SerializeField] Transform targetPosition;
 	[SerializeField] float speed;
+	[SerializeField] float maxSpeed;
 
+	Vector3 direction;
 
 	Rigidbody rb;
 
@@ -21,29 +20,39 @@ public class Move : MonoBehaviour
 
 	void Update()
 	{
-		if (Input.GetMouseButtonDown(0))
+		AvoidObstacles();
+		if (Input.GetMouseButton(0))
 		{
+			SteerBehaviour();
 
-			FollowTarget();
 		}
-	}
 
-	void FollowTarget()
+	}
+	void SteerBehaviour()
 	{
-		Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hitInfo;
-		if (Physics.Raycast(ray, out hitInfo, ground))
+		direction = (targetPosition.position - transform.position).normalized;
+		rb.velocity += direction * speed;
+		if (direction.magnitude > maxSpeed)
 		{
-			if(hitInfo.collider.tag == "Ground")
-            {
-                MovePlayer();
-            }
-
+			rb.velocity = direction.normalized * maxSpeed;
 		}
 	}
 
-    void MovePlayer()
-    {
-       
-    }
+	void AvoidObstacles()
+	{
+		direction = (targetPosition.position - transform.position).normalized;
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hit, 20))
+		{
+			if (hit.transform != transform)
+			{
+				direction += hit.normal * 100;
+			}
+		}
+
+		Quaternion rot = Quaternion.LookRotation(direction);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime);
+		transform.position += transform.forward * 20 * Time.deltaTime;
+	}
 }
